1a8b43385e47290024c30977e7916aae1ed1c1a9
Harsh kumar <116588175+HKSHUBHAM@users.noreply.github.com>
2024-01-02 12:25:42 +0000

    Data Types

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,26 @@
+// Day 2
+
+"use strict"; /* Treat all js code as newer version */
+
+// alter(3+3) /* we are using nodejs not brower so we will not be using this 
+
+// MDN, or tc39.es are the documentation for the javascript 
+
+// Now
+// DataTypes
+let name = "Harsh"
+let Age = 18
+let isloggedin = false
+
+// number = 2 to power 53
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value
+// underfined = not defined value 
+// symbol = unique
+// object
+
+console.log(typeof Age)
+console.log(typeof null)
+console.log(typeof undefined)
